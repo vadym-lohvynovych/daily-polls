0c386d8868b6a1dcc446bd6f5dbfb7b3b04198e1
vadym.lohvynovych <VADYM.LOHVYNOVYCH@kyivstar.net>
2024-11-30 17:38:36 +0200

    chat id from env

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const { Telegraf } = require('telegraf');
 require('dotenv').config();
 
 const bot = new Telegraf(process.env.TOKEN);
-const chatId = '-4536195920';
-
 
 async function sendPoll() {
     try {
-        const poll = await bot.telegram.sendPoll(chatId, 'Вдалося вчора почитати?)', ['Так, звісно!', 'Ні((('], { is_anonymous: false });
+        const poll = await bot.telegram.sendPoll(process.env.CHAT_ID, 'Вдалося вчора почитати?)', ['Так, звісно!', 'Ні((('], { is_anonymous: false });
         console.log('done')
     } catch (e) {
         console.log('error')
