bd515748792deefd7282ede0e0412da6865d99c0
vadym.lohvynovych <VADYM.LOHVYNOVYCH@kyivstar.net>
2024-12-03 10:25:54 +0200

    update text

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bot = new Telegraf(process.env.TOKEN);
 
 async function sendPoll() {
     try {
-        await bot.telegram.sendPoll(process.env.CHAT_ID, 'Вдалося вчора почитати?)', ['Листаю сторінки як стрічку в інстаграмі', 'Ні(((', 'Не беру участь'], { is_anonymous: false });
+        await bot.telegram.sendPoll(process.env.CHAT_ID, 'Вдалося вчора почитати?)', ['Так!', 'Ні(((', 'Не беру участь'], { is_anonymous: false });
         await bot.telegram.sendPoll(process.env.CHAT_ID, 'А медитувати?)', ['Намасте!', 'Ні(((', 'Не беру участь'], { is_anonymous: false });
         console.log('done')
     } catch (e) {
